9e20ef9a5718321ff7d313b3a98152e353dc6842
amolg417 <139610451+amolg417@users.noreply.github.com>
2024-10-05 17:43:05 +0530

    Update Navbar.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -15,20 +15,6 @@ const Navbar = () => {
         >
           Amol Gaikwad
         </motion.span>
-        {/* <div className="social">
-          <a href="#">
-            <img src="/facebook.png" alt="" />
-          </a>
-          <a href="#">
-            <img src="/instagram.png" alt="" />
-          </a>
-          <a href="#">
-            <img src="/youtube.png" alt="" />
-          </a>
-          <a href="#">
-            <img src="/dribbble.png" alt="" />
-          </a>
-        </div> */}
       </div>
     </div>
   );
